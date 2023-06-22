88a11cb811127d43228830d3581a57580ceb5da5
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-06-21 22:27:58 -0500

    inicio

--- src/component/App.js
+++ src/component/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './home';
 import Cart from './cart';
 import Products from './product';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/products" component={Products} />
-          <Route path="/cart" component={Cart} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
       </div>
     </Router>
   );
